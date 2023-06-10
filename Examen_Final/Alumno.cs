95a42861930d45968c3a8b3bc9ee1feafe2d1c3d
Hinata1411 <85373964+Hinata1411@users.noreply.github.com>
2023-06-10 10:51:41 -0600

    Examen Final

---
+++ Examen_Final/Alumno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Final
+{
+    internal class Alumno
+    {
+        private string Carnet {get;set;}
+        private string Nombre { get;set;}
+        private string Telefono { get;set;}
+        private string Grado { get; set; }
+
+
+
+    }
+}
